8fff6054b1ca3df6c755fbdc25d6c89d22c080ad
Mohammed Saifuddin <65835141+mohammed-saifuddin@users.noreply.github.com>
2026-06-15 22:48:26 +0530

    Update test to check weather app heading

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders weather app heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  
+  const headingElement = screen.getByText(/Know the weather of your city/i);
+  
+  expect(headingElement).toBeInTheDocument();
 });
